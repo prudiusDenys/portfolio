0d6c6dda8260ca07de850fadaeebea7a3d95a354
prudiusDenys <prdslife@gmail.com>
2020-09-19 18:00:48 +0200

    Navbar fix

--- src/Components/Header/Navbar/Navbar.tsx
+++ src/Components/Header/Navbar/Navbar.tsx
@@ -10,15 +10,15 @@ export const Navbar = () => {
 		<div className={classes.navbar}>
 			<ul className={classes.list}>
 				{/*home*/}
-				<li className={classes.active}><NavLink to="/"><FontAwesomeIcon className={classes.icon} icon={faHome}/></NavLink></li>
+				<li><NavLink exact to="/" activeClassName={classes.active}><FontAwesomeIcon className={classes.icon}  icon={faHome}/></NavLink></li>
 				{/*About Me*/}
-				<li><NavLink to="/about"><FontAwesomeIcon className={classes.icon} icon={faAddressCard}/></NavLink></li>
+				<li><NavLink to="/about" activeClassName={classes.active}><FontAwesomeIcon className={classes.icon} icon={faAddressCard}/></NavLink></li>
 				{/*Skills*/}
-				<li><NavLink to="/skills"><FontAwesomeIcon className={classes.icon} icon={faUserCog}/></NavLink></li>
+				<li><NavLink to="/skills" activeClassName={classes.active}><FontAwesomeIcon className={classes.icon} icon={faUserCog}/></NavLink></li>
 				{/*Projects*/}
-				<li><NavLink to="/projects"><FontAwesomeIcon className={classes.icon} icon={faBriefcase}/></NavLink></li>
+				<li><NavLink to="/projects" activeClassName={classes.active}><FontAwesomeIcon className={classes.icon} icon={faBriefcase}/></NavLink></li>
 				{/*Contacts*/}
-				<li><NavLink to="/contacts"><FontAwesomeIcon className={classes.icon} icon={faEnvelopeOpen}/></NavLink></li>
+				<li><NavLink to="/contacts" activeClassName={classes.active}><FontAwesomeIcon className={classes.icon} icon={faEnvelopeOpen}/></NavLink></li>
 			</ul>
 		</div>
 	)
