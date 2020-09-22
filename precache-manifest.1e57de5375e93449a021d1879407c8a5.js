52b90e74e48a9a3d3c56b0b9e631bc395dfdc45c
prudiusDenys <prdslife@gmail.com>
2020-09-23 00:07:23 +0200

    Updates

---
+++ precache-manifest.1e57de5375e93449a021d1879407c8a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e19aedbbc07173ca8f7549f540475e3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9c06da9febd407f99dda",
+    "url": "/portfolio/static/css/main.9e379ae2.chunk.css"
+  },
+  {
+    "revision": "e55501a9d755f0b8790a",
+    "url": "/portfolio/static/js/2.fe64e553.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.fe64e553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c06da9febd407f99dda",
+    "url": "/portfolio/static/js/main.a2d0dc21.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "34d6473642fea65342c9016c9fa7347a",
+    "url": "/portfolio/static/media/counter.34d64736.jpg"
+  },
+  {
+    "revision": "d42bbc316ff08e3867d363fd7e66c36b",
+    "url": "/portfolio/static/media/myPhoto.d42bbc31.jpg"
+  },
+  {
+    "revision": "f8e8bf1bac8064c78adfb29388049495",
+    "url": "/portfolio/static/media/social.f8e8bf1b.jpg"
+  },
+  {
+    "revision": "694ac75bca4cc83e1c89fc1da0c10e6b",
+    "url": "/portfolio/static/media/todolist.694ac75b.jpg"
+  }
+]);
